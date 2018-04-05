12e257a3c24741ebcca924baae3a724069d5f8f2
姚小玉 <yaoxiaoyu@yaoxiaoyudeMacBook-Air.local>
2018-04-05 13:19:56 +0800

    完成第一个测试函数

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var collection_b;
+  for(var i in collection_b){
+    if(i % 2 == 0){
+      collection_b.push(i);
+    }
+  }//在这里写入代码
+  return collection_b;
 }
 
 module.exports = collect_all_even;
