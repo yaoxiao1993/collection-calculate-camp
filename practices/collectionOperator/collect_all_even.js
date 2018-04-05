f030b4d964637601a3b0d10800d890d3fc42dc5e
姚小玉 <yaoxiaoyu@yaoxiaoyudeMacBook-Air.local>
2018-04-05 13:56:39 +0800

    调试

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,13 +2,12 @@
 
 function collect_all_even(collection) {
   var collection_b;
-  for(var i in collection_b){
+  for(var i in collection){
     if(i % 2 == 0){
-      collection_b.push(i);
+      collection_b.push(collection[i]);
     }
   }//在这里写入代码
   return collection_b;
-  console.log(collection_b);
 }
 
 module.exports = collect_all_even;
