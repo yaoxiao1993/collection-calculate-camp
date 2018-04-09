1c6e2f2a8f55aa531c41020c4335863b03868e71
姚小玉 <yaoxiaoyu@yaoxiaoyudeMacBook-Air.local>
2018-04-09 23:07:43 +0800

    ../../../specs/section_2/practice_1_spec.js

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,18 +1,35 @@
 function count_same_elements(collection) {
-  var obj = {key:'a', count:0};
   var result_collection = [];
+  var obj = {};
   for(var i in collection){
-    if(collection[i] === obj['key']){
-      obj['count'] ++;
+    if(!obj[collection[i]]){
+      obj[collection[i]] = 1;
     }else{
-      result_collection.push(obj);
-      obj['key'] = collection[i];
-      count = 0;
-    }
-    
+      obj[collection[i]] ++;
+    } 
   }
+  for(var i in obj){
+    var obj_in_array = {};
+    obj_in_array.key = i;
+    obj_in_array.count = obj[i];
+    result_collection.push(obj_in_array);
+  }
+  return result_collection;
+
+  // var result_collection =[];
+  // for(var i in collection){
+  //   if(!result_collection[collection[i]]){
+  //     result_collection[collection[i]]={};
+  //     result_collection[collection[i]].key = collection[i];
+  //     result_collection[collection[i]].count =1;
+  //   }else{
+  //     result_collection[collection[i]].count++;
+  //   }
+  // }
+
+  // console.log(result_collection);
+  // return result_collection;
   
-  return obj;
   //在这里写入代码
 }
 
